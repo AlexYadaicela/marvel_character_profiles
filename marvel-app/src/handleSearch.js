7f8e9b368191b08eadae700d3be990ce2fb7fc4c
spycrab <wialex121@gmail.com>
2025-03-06 15:47:55 -0500

    Feat: add search handle to find characters

---
+++ marvel-app/src/handleSearch.js
@@ -0,0 +1,28 @@
+
+
+export async function setUpSearchBar(keyword){
+    const searchResults = document.getElementById('search_results');  
+    try{
+        const response = await fetch(`/api/marvel/search?name=${keyword}`);
+        if(!response.ok){
+            throw new Error(`Response status:${response.status} `); 
+        }
+        const res = await response.json(); 
+        const characters = res.data.results; 
+
+        const fragment = document.createDocumentFragment(); 
+        const ulElement = document.createElement('ul'); 
+        characters.forEach((character) => {
+            const liElement = document.createElement('li'); 
+            liElement.textContent = character.name;  
+
+            ulElement.appendChild(liElement); 
+        });
+
+        fragment.appendChild(ulElement); 
+        searchResults.appendChild(fragment); 
+
+    }catch(error){
+        console.error(error.message); 
+    }
+}
